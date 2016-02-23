60c2cc3894baac98165a30e74551008c162f64b9
lilianshuan <guodaojob@163.com>
2016-02-23 22:35:56 +0800

    增加字段

--- crm-api/src/main/java/net/gddata/other/core/Customer.java
+++ crm-api/src/main/java/net/gddata/other/core/Customer.java
@@ -2,11 +2,20 @@ package net.gddata.other.core;
 
 import lombok.Data;
 
+import java.util.Date;
+
+
 /**
  * Created by knix on 16/2/17.
  */
 @Data
 public class Customer {
     private Integer id;
-    private String name;
+    private String name; //姓名
+    private String gender; //性别
+    private String birthday; //生日
+    private String address;//住址
+    private String telephone; //电话
+    private String email;//邮箱
+    private Date enroll;//注册日期
 }
