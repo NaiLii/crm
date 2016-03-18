87cfe13e64e2e947e094c10c384d240f037990b9
lilianshuan <vs2@live.cn>
2016-03-18 23:31:26 +0800

    支持按年显示工作量

--- tools/src/main/java/net/gddata/other/tools/DateTime/Common.java
+++ tools/src/main/java/net/gddata/other/tools/DateTime/Common.java
@@ -1,7 +1,5 @@
 package net.gddata.other.tools.DateTime;
 
-import javafx.beans.DefaultProperty;
-
 import java.util.Calendar;
 import java.util.Date;
 
@@ -9,14 +7,34 @@ import java.util.Date;
  * Created by knix on 16/3/7.
  */
 public class Common {
-    public static Integer Toyear(){
+    public static Integer Toyear() {
         Calendar instance = Calendar.getInstance();
         return instance.get(Calendar.YEAR);
     }
 
-    public static Integer getYear(Date date){
+    public static Integer getYear(Date date) {
         Calendar instance = Calendar.getInstance();
         instance.setTime(date);
         return instance.get(Calendar.YEAR);
+    }
+
+    public static Date getNewYearDay() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_YEAR, 0);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
+    }
+
+    public static Calendar getNewYearDayCalendar(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_YEAR, 0);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar;
     }
 }
